fa5e937eea1eabcd6f87139419162177b6b14cc8
Blackscure <Wekesabuyahi@gmail.com>
2018-08-12 13:23:47 +0300

    my files

---
+++ src/app.py
@@ -0,0 +1,27 @@
+from flask import Flask,render_template
+from data import Articles
+
+app = Flask(__name__)
+
+Articles = Articles()
+
+@app.route('/')
+def home():
+    return render_template( "home.html")
+
+@app.route('/about')
+def about():
+    return render_template( "about.html")
+
+@app.route('/articles')
+def articles():
+    return render_template( "articles.html", articles = Articles)
+
+@app.route('/article/<string:id>/')
+def articles_template(id):
+    return render_template( "article.html", id = id)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ src/data.py
@@ -0,0 +1,28 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article one',
+            'body': 'hello mama Africa how are you am feeling good and i hope you are too',
+            'author': 'Blackscure',
+            'created_date': '04-25-2018'
+        },
+
+            {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'hello mama Africa how are you am feeling good and i hope you are too',
+            'author': 'cure',
+            'created_date': '07-2-2018'
+        },
+
+           {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'hello mama Africa how are you am feeling good and i hope you are too',
+            'author': 'Blacks',
+            'created_date': '04-2-2018'
+        },
+
+    ]
+    return articles
